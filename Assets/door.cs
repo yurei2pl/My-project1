4826b270587b4c470e5d0593b89fffe119ff3cdb
Ghost <yurei2pl@gmail.com>
2026-05-26 22:14:08 +0200

    Initial commit

---
+++ Assets/door.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    public float openAngle = 75f;
+    public float speed = 2f;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    private bool playerNearby = false;
+
+    void Start()
+    {
+        closedRotation = transform.rotation;
+
+        openRotation = Quaternion.Euler(
+            transform.eulerAngles + new Vector3(0, openAngle, 0)
+        );
+    }
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            isOpen = !isOpen;
+        }
+
+        Quaternion targetRotation = isOpen ? openRotation : closedRotation;
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            Time.deltaTime * speed
+        );
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+        }
+    }
+}
