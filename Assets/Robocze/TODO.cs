2b17f20653c54d890cca39ef480364ba52055e32
The Griffin <wastcott7@yahoo.com>
2026-07-27 10:13:59 +0200

    Update TODO.cs

--- Assets/Robocze/TODO.cs
+++ Assets/Robocze/TODO.cs
@@ -5,9 +5,9 @@ public class TODO
     /*
     TODO:
     [ ] Poprawić mechanizm otwierana drzwi (Konrad)
-    
-    
-    
+    [ ] Wyskakujący guzik interakcji po zderzeniu czołowym z drzwiami (Konrad)
+    [ ] Dialog po interakcji z rozdzielnicą elektryczną (Konrad(Mechanizm);David(Dialog))
+    [ ] Latarka jebie w oczy, sprawdzić (Dawid)
     
     
     
