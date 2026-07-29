919a139a6d4fbf61f76c4fd09711758af23dbefa
Ghost <yurei2pl@gmail.com>
2026-07-29 20:32:57 +0200

    Poprawiona latarka, TODO

--- Assets/Robocze/TODO.cs
+++ Assets/Robocze/TODO.cs
@@ -5,6 +5,9 @@ public class TODO
     /*
     TODO:
     [ ] Poprawić mechanizm otwierana drzwi (Konrad)
+    [ ] Zrobić mechanizm (chwilowego) podnoszenia i obracania przedmiotów (Konrad)
+    [ ] Zrobić model/teksture/treść karteczek fabularnych (Dawid)
+    [ ] Zrobić nowy model drzwi (Dawid)
     
     
     
